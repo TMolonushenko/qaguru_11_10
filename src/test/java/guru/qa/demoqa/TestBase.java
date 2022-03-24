1c8d0495c367b9426e0458653a945253c86d0647
tmolonushenko <tmolonushenko@gmail.com>
2022-03-24 23:29:42 +0300

    added selenide

--- src/test/java/guru/qa/demoqa/TestBase.java
+++ src/test/java/guru/qa/demoqa/TestBase.java
@@ -2,6 +2,7 @@ package guru.qa.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
@@ -9,8 +10,10 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = " https://demoqa.com/";
         Configuration.browserSize = "1920x1080";
-
+        DesiredCapabilities capabilities  = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability(" enableVideo", false);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
-
-
 }
